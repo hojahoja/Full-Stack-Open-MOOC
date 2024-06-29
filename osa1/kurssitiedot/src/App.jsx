64ebe8f1aba234203f06f04442e2aaf9d0750d6a
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-06-29 11:14:07 +0300

    osa 1.4

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -51,29 +51,14 @@ const App = () => {
       exercises: 14,
     },
   ];
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const names = parts.map((p) => p.name);
+  const exerciseCounts = parts.map((p) => p.exercises);
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        exercises={[part1.exercises, part2.exercises, part3.exercises]}
-        parts={[part1.name, part2.name, part3.name]}
-      />
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content exercises={exerciseCounts} parts={names} />
+      <Total exercises={exerciseCounts} />
     </div>
   );
 };
