c2b2b6900ff6c69b7fb3a0d98a19a40a17c0779c
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-07-11 18:43:24 +0300

    4.2: blogilista, step2

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,32 +2,21 @@ const express = require("express");
 const app = express();
 //const cors = require("cors");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
 const { MONGODB_URI } = require("./utils/config");
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch((error) => logger.error("Error connecting to MongoDB:", error.message));
 
-const Blog = mongoose.model("Blog", blogSchema);
-mongoose.connect(MONGODB_URI);
+morgan.token("body", (req) => (req.method === "POST" ? JSON.stringify(req.body) : undefined));
 
 //app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
