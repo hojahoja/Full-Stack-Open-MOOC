b785ddc78c63e325ef180d1999aae82f1e46a5f4
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-07-14 21:31:21 +0300

    4.13-4.14 blogilistan laajennus, step1-2

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,20 +1,34 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", async (request, response) => {
+blogsRouter.get("/", async (req, res) => {
   const blogs = await Blog.find({});
-  response.json(blogs);
+  res.json(blogs);
 });
 
-blogsRouter.get("/:id", async (request, response) => {
-  const blog = await Blog.findById(request.params.id);
-  blog ? response.json(blog) : response.status(404).end();
+blogsRouter.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  blog ? res.json(blog) : res.status(404).end();
 });
 
-blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.post("/", async (req, res) => {
+  const blog = new Blog(req.body);
   const result = await blog.save();
-  response.status(201).json(result);
+  res.status(201).json(result);
+});
+
+blogsRouter.delete("/:id", async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
+
+blogsRouter.put("/:id", async (req, res) => {
+  const updatedNote = await Blog.findByIdAndUpdate(
+    req.params.id,
+    { $inc: { likes: 1 } },
+    { new: true, runValidators: true, context: "query" }
+  );
+  updatedNote ? res.json(updatedNote) : res.status(404).end();
 });
 
 module.exports = blogsRouter;
--- osa4/blogilista/tests/blogs-api.test.js
+++ osa4/blogilista/tests/blogs-api.test.js
@@ -37,6 +37,15 @@ describe("when bloglist api already has some blogs", () => {
 
       assert.deepStrictEqual(resultBlog.body, expectedBlog);
     });
+
+    test("returns 404 Not found with nonexistent id", async () => {
+      const nonexistentID = "669372b942d28ca379809a3c";
+      await api.get(`/api/blogs/${nonexistentID}`).expect(404);
+    });
+
+    test("returns 400 Bad request with invalid id", async () => {
+      await api.get("/api/blogs/123213213").expect(400);
+    });
   });
   describe("addition of a new blog", () => {
     const postBlog = async (blog, expectedStatusCode) => {
@@ -95,6 +104,75 @@ describe("when bloglist api already has some blogs", () => {
       };
 
       await postBlog(someBlog, 400);
+    });
+  });
+  describe("removal of a specific blog", () => {
+    test("succeeds with a valid id", async () => {
+      const blogsAtStart = await helper.blogsInDb();
+      const deletedBlog = blogsAtStart[0];
+
+      await api.delete(`/api/blogs/${deletedBlog.id}`).expect(204);
+
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1);
+
+      const containsDeletedBlog = blogsAtEnd.reduce(
+        (acc, blog) => (acc ? true : blog.id === deletedBlog.id),
+        false
+      );
+      assert.strictEqual(containsDeletedBlog, false);
+    });
+
+    const dbUnchangedWithID = async (testID, expectedStatus) => {
+      const blogsAtStart = await helper.blogsInDb();
+
+      await api.delete(`/api/blogs/${testID}`).expect(expectedStatus);
+
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(blogsAtEnd.length, blogsAtStart.length);
+    };
+
+    test("doesn't change list size with a nonexistent id", async () => {
+      const nonexistentID = "669372b942d28ca379809a3c";
+      await dbUnchangedWithID(nonexistentID, 204);
+    });
+
+    test("returns 400 Bad Request with invalid id", async () => {
+      const invalidID = "123213123";
+      await dbUnchangedWithID(invalidID, 400);
+    });
+  });
+  describe("updating a specific blog with put", () => {
+    test("increments the number of likes by one", async () => {
+      const blogsAtStart = await helper.blogsInDb();
+
+      await api.put(`/api/blogs/${blogsAtStart[0].id}`).expect(200);
+
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(blogsAtEnd[0].likes, blogsAtStart[0].likes + 1);
+    });
+    test("doesn't change any other value than likes", async () => {
+      const blogsAtStart = await helper.blogsInDb();
+      const changedBlog = {
+        ...blogsAtStart[0],
+        title: "u got pwned",
+        author: "script kiddie",
+        url: "lol.com",
+        likes: 666,
+      };
+      await api.put(`/api/blogs/${blogsAtStart[0].id}`).send(changedBlog).expect(200);
+
+      const blogsAtEnd = await helper.blogsInDb();
+
+      const { likes: originalLikes, ...originalEntries } = blogsAtStart[0];
+      const { likes: postUpdateLikes, ...postUpdateEntries } = blogsAtEnd[0];
+
+      assert.strictEqual(postUpdateLikes, originalLikes + 1);
+      assert.deepStrictEqual(postUpdateEntries, originalEntries);
+    });
+    test("returns 404 Not found with nonexistent id", async () => {
+      const nonexistentID = "669372b942d28ca379809a3c";
+      await api.put(`/api/blogs/${nonexistentID}`).expect(404);
     });
   });
 });
