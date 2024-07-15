7d8dac6184b0f76511b30f11b45e20128856d72b
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-07-15 14:20:17 +0300

    4.15: blogilistan laajennus, step3

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ require("express-async-errors");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const { MONGODB_URI } = require("./utils/config");
@@ -23,6 +24,7 @@ if (process.env.NODE_ENV !== "test") {
 }
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
---
+++ osa4/blogilista/controllers/users.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcryptjs");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+usersRouter.post("/", async (req, res) => {
+  const { username, name, password } = req.body;
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = new User({ username, name, passwordHash });
+  const savedUser = await user.save();
+
+  res.status(201).json(savedUser);
+});
+
+//Temporary testing method
+usersRouter.delete("/", async (req, res) => {
+  await User.deleteMany({});
+  res.status(418).end();
+});
+
+module.exports = usersRouter;
