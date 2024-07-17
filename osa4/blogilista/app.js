ef0bd72ac9a7b1bfde50e76996adf86efbc95129
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-07-17 15:19:42 +0300

    4.18: blogilistan laajennus, step6

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const { MONGODB_URI } = require("./utils/config");
@@ -18,13 +19,16 @@ mongoose
 //app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV === "dev") {
   morgan.token("body", (req) => (req.method === "POST" ? JSON.stringify(req.body) : undefined));
   app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
+} else if (process.env.NODE_ENV !== "test") {
+  app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 }
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
