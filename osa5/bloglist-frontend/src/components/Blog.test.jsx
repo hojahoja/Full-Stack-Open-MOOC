d9b2bab13a82e08820d9c6b369cdd2bdbddd4a9c
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-07-24 18:32:25 +0300

    5.15: blogilistan testit, step3

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,19 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
-import { beforeEach } from "vitest";
+import { beforeEach, expect } from "vitest";
 import userEvent from "@testing-library/user-event";
 
 describe("<Blog />", () => {
   let container;
+  let updateMock;
 
   beforeEach(() => {
+    updateMock = vi.fn();
+
     const blog = {
       title: "Test title!",
       author: "testerMan",
       url: "http://www.testerBester.com/",
       likes: 1,
     };
-    container = render(<Blog blog={blog} />).container;
+    container = render(<Blog blog={blog} updateBlog={updateMock} />).container;
   });
 
   test("should always render blog title", () => {
@@ -29,5 +32,17 @@ describe("<Blog />", () => {
     element = screen.getByText("testerMan", { exact: false });
     element = screen.getByText("http://www.testerBester.com/", { exact: false });
     element = screen.getByText("likes 1", { exact: false });
+  });
+
+  test("prop updateBlog should get activated twice when clicking like twice", async () => {
+    const clicker = userEvent.setup();
+    const viewButton = screen.getByText("view");
+    await clicker.click(viewButton);
+
+    const likeButton = screen.getByText("like");
+    await clicker.click(likeButton);
+    await clicker.click(likeButton);
+
+    expect(updateMock.mock.calls).toHaveLength(2);
   });
 });
