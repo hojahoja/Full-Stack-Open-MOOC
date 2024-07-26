d031f0159e0b8b6077b70877d1e905dddf285b8f
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-07-26 14:41:20 +0300

    5.18: blogilistan end to end ‑testit, step2

--- osa5/playwright/tests/bloglist.spec.js
+++ osa5/playwright/tests/bloglist.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { blogListLogin } = require("./helper");
 
 describe("Note app", () => {
   beforeEach(async ({ page, request }) => {
@@ -16,5 +17,21 @@ describe("Note app", () => {
 
   test("Login form is shown", async ({ page }) => {
     await expect(page.getByText("log in to application")).toBeVisible();
+  });
+
+  describe("logging in", () => {
+    test("should fail with incorrect credentials", async ({ page }) => {
+      await blogListLogin(page, "nobody", "incorrect");
+
+      const errorBox = page.getByText("invalid username or password");
+      await expect(errorBox).toBeVisible();
+      await expect(errorBox).toHaveCSS("border-color", "rgb(255, 0, 0)");
+    });
+
+    test("should succeed with incorrect credentials", async ({ page }) => {
+      await blogListLogin(page, "nobody", "passwordsAreForLosers");
+
+      await expect(page.getByText("John Doe logged in")).toBeVisible();
+    });
   });
 });
---
+++ osa5/playwright/tests/helper.js
@@ -0,0 +1,7 @@
+const blogListLogin = async (page, username, password) => {
+  await page.locator('input[name="Username"]').fill(username);
+  await page.locator('input[name="Password"]').fill(password);
+  await page.getByRole("button", { name: "login" }).click();
+};
+
+export { blogListLogin };
