66c6d4040711b1d01cc2b2d22e39de9a6adf54b5
hojahoja <5722145+hojahoja@users.noreply.github.com>
2024-07-31 15:00:49 +0300

    7.4: anekdoottisovellus ja hookit step1

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Route, Link, Routes, useMatch, useNavigate } from "react-router-dom";
+import useField from "./hooks";
 
 const Menu = () => {
   const padding = {
@@ -77,21 +78,22 @@ const Footer = () => (
 );
 
 const CreateNew = ({ addNew, showNotification }) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("content");
+  const author = useField("author");
+  const info = useField("info");
+
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     navigate("/");
-    showNotification(`a new anecdote ${content} created !`);
+    showNotification(`a new anecdote ${content.value} created!`);
   };
 
   return (
@@ -100,15 +102,15 @@ const CreateNew = ({ addNew, showNotification }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name="content" value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name="info" value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
